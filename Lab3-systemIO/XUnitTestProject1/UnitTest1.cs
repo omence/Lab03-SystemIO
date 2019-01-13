8fa13f228c261a54100dab36f1114fa2484ae7e4
Jason Few <omence11@gmail.com>
2019-01-12 16:08:50 -0800

    Tests almost done

--- Lab3-systemIO/XUnitTestProject1/UnitTest1.cs
+++ Lab3-systemIO/XUnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using System.IO;
 using Lab3SystemIO;
 
 namespace XUnitTestProject1
@@ -8,18 +9,63 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [Fact]
-        public void CanretrieveAllWordsFromFIle()
+        public void CanViewAllWordsWorks()
         {
             //arrange
             string path = "../../../hangman.txt";
 
             //act
-            //Program.ViewAllWords(path);
+            string index = Program.ViewAllWords(path);
 
             //assert
-            Assert.Contains("DOG", Program.ViewAllWords(path));
+            Assert.Equal("DOG", index);
 
         }
-       
+
+        [Fact]
+        public void AddWordWorks()
+        {
+            //arrange
+            string path = "../../../hangman.txt";
+            string userAdd = "CAT";
+            //act
+            string index = Program.AddToFile(path, userAdd);
+
+            //assert
+            Assert.Contains("CAT", userAdd);
+
+        }
+
+        [Fact]
+        public void DeleteWordWorks()
+        {
+            //arrange
+            string path = "../../../hangman.txt";
+            string[] wordsInFile = File.ReadAllLines(path);
+            string userDelete = "BAG";
+            
+            //act
+            Program.DeleteFromFile(path, userDelete);
+
+            //assert
+            Assert.DoesNotContain("BAG", wordsInFile);
+
+        }
+
+        [Fact]
+        public void CheckLetterWorks()
+        {
+            //arrange
+            string path = "../../../hangman.txt";
+            string word = "BUT";
+
+            //act
+            Program.Play(path);
+
+            //assert
+            Assert.Contains("U", word);
+
+        }
+
     }
 }
